b6051659cd7243bdb4b72ca7f6b29d43402025cf
johnloydmunoz2002 <johnloydmunoz0110@gmail.com>
2024-05-25 19:18:13 +0800

    Update Index.js

--- Index.js
+++ Index.js
@@ -6,3 +6,12 @@ function clickLike1(){
   countLike1.textContent = totalLikes.toString()
 }
 btnLike1.addEventListener("click",clickLike1)
+
+const btnLike2 = document.getElementById("btnLike2")
+const countLike2 = document.getElementById("countLike2")
+
+function clickLike2(){
+  let totalLikes = parseInt(countLike2.value) + 1
+  countLike2.textContent = totalLikes.toString()
+}
+btnLike2.addEventListener("click",clickLike2)
