f9bd661b59886b535db104ec49d6ccb372e3b332
johnloydmunoz2002 <johnloydmunoz0110@gmail.com>
2024-06-08 17:48:04 +0800

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,17 +1,17 @@
-const btnLike1 = document.getElementById("btnLike1")
-const countLike1 = document.getElementById("countLike1")
+const btnDisLike1 = document.getElementById("btnDisLike1")
+const countDisLike1 = document.getElementById("countDisLike1")
 
-function clickLike1(){
-  let totalLikes = parseInt(countLike1.value) + 1
-  countLike1.textContent = totalLikes.toString()
-}
-btnLike1.addEventListener("click",clickLike1)
+function clickDisLike1(){
+  let totalDisLikes = parseInt(countDisLike1.value) + 1
+  countDisLike1.textContent = totalDisLikes.toString()
+}  
+btnDisLike1.addEventListener("click",clickDisLike1)
 
-const btnLike2 = document.getElementById("btnLike2")
-const countLike2 = document.getElementById("countLike2")
+const btnDisLike2 = document.getElementById("btnDisLike2")
+const countDisLike2 = document.getElementById("countDisLike2")
 
-function clickLike2(){
-  let totalLikes = parseInt(countLike2.value) + 1
-  countLike2.textContent = totalLikes.toString()
-}
-btnLike2.addEventListener("click",clickLike2)
+function clickDisLike2(){
+  let totalDisLikes = parseInt(countDisLike2.value) + 1
+  countDisLike2.textContent = totalDisLikes.toString()
+}  
+btnDisLike2.addEventListener("click",clickDisLike2)
